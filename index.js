36dd2e61438a51f94771cfc74dbea0fbc47819f5
bisma <150579746+bismanaeem28@users.noreply.github.com>
2024-03-08 23:56:41 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+async function startfunc() {
+    const systemNum = Math.floor(Math.random() * 10);
+    const userNum = await inquirer.prompt({
+        type: "number",
+        name: "userGuess",
+        message: "Enter Your Number Between 1 - 10: "
+    });
+    const { userGuess } = userNum;
+    console.log("Your Guess:", userGuess, "\nSystem's Guess:", systemNum);
+    if (userGuess === systemNum) {
+        console.log("Your Guess Is Correct\nYou Won!");
+    }
+    else {
+        console.log("Better Luck Next Time");
+    }
+}
+async function startAgain() {
+    do {
+        await startfunc();
+        var again = await inquirer.prompt({
+            type: "input",
+            name: "restart",
+            message: "Do You Want To Continue? Press Y or N"
+        });
+    } while (again.restart == "Y" || again.restart == "YES");
+}
+startAgain();
